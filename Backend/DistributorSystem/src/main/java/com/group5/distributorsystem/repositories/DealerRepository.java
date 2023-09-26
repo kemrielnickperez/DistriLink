f8361090f44e79b092358a0ce9fb8b10999a775d
ChaRam05 <charmainemae.ramirez@cit.edu>
2023-09-27 07:48:38 +0800

    mongodb implementation - backend - changes for repository  classes

--- Backend/DistributorSystem/src/main/java/com/group5/distributorsystem/repositories/DealerRepository.java
+++ Backend/DistributorSystem/src/main/java/com/group5/distributorsystem/repositories/DealerRepository.java
@@ -1,10 +1,12 @@
 package com.group5.distributorsystem.repositories;
 
 import com.group5.distributorsystem.models.Dealer;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DealerRepository extends CrudRepository<Dealer, Integer> {
+public interface DealerRepository extends MongoRepository<Dealer, String> {
 
 }
