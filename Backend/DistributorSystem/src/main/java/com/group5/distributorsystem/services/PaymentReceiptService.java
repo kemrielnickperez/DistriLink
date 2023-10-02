d3b225d72fdd38c8d0f4950f3f66f1e6f98ed36b
ChaRam05 <charmainemae.ramirez@cit.edu>
2023-10-02 15:34:06 +0800

    new commit on payment recording (working)

--- Backend/DistributorSystem/src/main/java/com/group5/distributorsystem/services/PaymentReceiptService.java
+++ Backend/DistributorSystem/src/main/java/com/group5/distributorsystem/services/PaymentReceiptService.java
@@ -24,17 +24,7 @@ public class PaymentReceiptService {
        return paymentReceiptRepository.findAll();
     }
 
-    //di rako need ani kay sa list of receipts kay nagsagol raman tanan
-   /* public Iterable<DirectPaymentReceipt> getPaymentReceiptsByDiscriminatorValue(String discriminatorvalue){
-        return paymentReceiptRepository.getPaymentReceiptsByDiscriminatorValue(discriminatorvalue);
-    }*/
-
-    //I decided nga dili nalang ibutang diri nag methods para dili samok
-    /*public DirectPaymentReceipt createDirectPaymentReceipt(DirectPaymentReceipt directPaymentReceipt){
-
-        return paymentReceiptRepository.save(directPaymentReceipt);
-    }*/
-
+    
     public Optional<PaymentReceipt> getPaymentReceiptByID(String paymentreceiptid){
         return paymentReceiptRepository.findById(paymentreceiptid);
     }
