8ea6799aabe571b7cbc9cc2fa01437205ec00bbe
ChaRam05 <charmainemae.ramirez@cit.edu>
2023-10-18 00:46:18 +0800

    fixed the cross origin of dealer document

--- Backend/DistributorSystem/src/main/java/com/group5/distributorsystem/controllers/DealerController.java
+++ Backend/DistributorSystem/src/main/java/com/group5/distributorsystem/controllers/DealerController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin
 @RequestMapping("/dealer")
 public class DealerController {
 
